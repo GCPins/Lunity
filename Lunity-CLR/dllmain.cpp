f62f9d1ec949988b8931e744670fbab097ba36d3
DisabledMallis <mallisiscool@gmail.com>
2020-02-20 00:32:34 -0500

    host returns ok so idk why its not running c# still

--- Lunity-CLR/dllmain.cpp
+++ Lunity-CLR/dllmain.cpp
@@ -2,7 +2,6 @@
 #include "pch.h"
 #include <metahost.h>
 #pragma comment(lib, "mscoree.lib")
-#include <iostream>
 #include <fstream>
 
 std::wstring s2ws(const std::string& s)
@@ -16,7 +15,18 @@ std::wstring s2ws(const std::string& s)
     delete[] buf;
     return r;
 }
+DWORD getAddress(DWORD baseAddress, DWORD offsets[], int offsetCount)
+{
+    DWORD address; // Where the final address will be stored
+    address = *(DWORD*)(baseAddress); // Add the base address to the modules base address.
 
+    // Loop through each offset
+    for (int i = 0; i < offsetCount; i++) {
+        address = *(DWORD*)(address + offsets[i]);
+    }
+
+    return address;
+}
 DWORD WINAPI startClr(LPVOID lpParam)
 {
 	ICLRMetaHost* metaHost = NULL; //Declare our CLR Meta Host value as a NULL
@@ -24,7 +34,7 @@ DWORD WINAPI startClr(LPVOID lpParam)
 	ICLRRuntimeHost* runtimeHost = NULL; //Delcare our CLR HOST as a NULL
 
 	if (CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (LPVOID*)&metaHost) == S_OK) //If Creating CLR Instance with follow parameters is successful
-		if (metaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, (LPVOID*)&runtimeInfo) == S_OK) //If getting Runtime version is successful
+		if (metaHost->GetRuntime(L"v4.8", IID_ICLRRuntimeInfo, (LPVOID*)&runtimeInfo) == S_OK) //If getting Runtime version is successful
 			if (runtimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, (LPVOID*)&runtimeHost) == S_OK) //If getting the interface with the follow parameters is successful
 				if (runtimeHost->Start() == S_OK) //Start the CLR (If it is successful)
 				{
@@ -32,9 +42,7 @@ DWORD WINAPI startClr(LPVOID lpParam)
 
 					//Invoke our method through CLR host using following parameters
 					std::string lunityPath = std::string(getenv("APPDATA")).c_str() + std::string("\\Lunity\\Lunity-Injectable.dll");
-					//runtimeHost->ExecuteInDefaultAppDomain(s2ws(lunityPath).c_str(), L"Lunity_Injectable.EntryClass", L"Main", L"Hello!", &pReturnValue);
-					runtimeHost->ExecuteInDefaultAppDomain(L"C:\\Users\\Mike\\AppData\\Roaming\\Lunity\\Lunity-Injectable.dll", L"Lunity_Injectable.EntryClass", L"Main", L"Hello!", &pReturnValue);
-
+					runtimeHost->ExecuteInDefaultAppDomain(s2ws(lunityPath).c_str(), L"Lunity_Injectable.EntryClass", L"Main", L"Hello!", &pReturnValue);
 				}
 	return 0;
 }
