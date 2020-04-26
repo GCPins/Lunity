bf3d7efaa10870a451f33cce141bab1ad760ebac
DisabledMallis <mallisiscool@gmail.com>
2020-04-25 23:23:27 -0400

    delete shit

--- Lunity/Lunity/Client/KeyHook.h
+++ Lunity/Lunity/Client/KeyHook.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../BigHead.h"
-#include "../polyhook2/CapstoneDisassembler.hpp"
+#include 
+#include <polyhook2/CapstoneDisassembler.hpp">
 #include "../polyhook2/Detour/x64Detour.hpp"
 #include "../polyhook2/Enums.hpp"
 class KeyHook
