855ffde1d52ea636d426e2cd60c8f534ef0ecd4e
DisabledMallis <mallisiscool@gmail.com>
2020-05-14 18:57:45 -0400

    Player coord shit

--- Lunity/Lunity/SDK/LunMC.h
+++ Lunity/Lunity/SDK/LunMC.h
@@ -1,5 +1,6 @@
-// Created with ReClass.NET 1.2 by KN4CK3R
 #pragma once
+// Created with ReClass.NET 1.2 by KN4CK3R
+
 class ClientInstance
 {
 public:
@@ -24,5 +25,11 @@ class LocalPlayer
 public:
 	char pad_0000[376]; //0x0000
 	bool OnGround; //0x0178
-	char pad_0179[655]; //0x0179
-}; //Size: 0x0408
+	char pad_0179[567]; //0x0179
+	char Type[8]; //0x03B0
+	char pad_03B8[160]; //0x03B8
+	float PosX; //0x0458
+	float PosY; //0x045C
+	float PosZ; //0x0460
+	char pad_0464[932]; //0x0464
+}; //Size: 0x0808
