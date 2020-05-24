f6e05d77ab61177bdcdf44bcb7cf37050d056fb3
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-05-24 17:19:17 +0100

    Update BunnyHop.cpp

--- Lunity/Lunity/Client/Cheats/BunnyHop.cpp
+++ Lunity/Lunity/Client/Cheats/BunnyHop.cpp
@@ -3,7 +3,7 @@
 #include "../Hooks/KeyHook.h"
 
 #define PI 3.14159
-float bunnyHopSpeed = 0.4f;
+float bunnyHopSpeed = 0.42f;
 
 BunnyHop::BunnyHop() :Cheat::Cheat("BunnyHop", "Movement")
 {
@@ -17,10 +17,6 @@ void BunnyHop::onLoop()
 void BunnyHop::onTick()
 {
 	Cheat::onTick();
-	//A = 0x41
-	//D = 0x44
-	//S = 0x53
-	//W = 0x57
 	if (LunMem::getClientInstance() != NULL) {
 		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
 			LocalPlayer* Player = LunMem::getClientInstance()->LocalPlayer;
