c0c59df6385ed0452358bb539ee3d196358505fd
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-05-27 05:16:07 +0100

    Update Gamemode.cpp

--- Lunity/Lunity/Client/Cheats/Gamemode.cpp
+++ Lunity/Lunity/Client/Cheats/Gamemode.cpp
@@ -9,10 +9,18 @@ Gamemode::Gamemode() :Cheat::Cheat("Gamemode", "Player")
 void Gamemode::onTick()
 {
 	Cheat::onTick();
-	LunMem::getClientInstance()->LocalPlayer->setPlayerGameType(1);
+	if (LunMem::getClientInstance() != NULL) {
+		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
+			LunMem::getClientInstance()->LocalPlayer->setPlayerGameType(1);
+		}
+	}
 }
 
 void Gamemode::onDisable() {
 	Cheat::onDisable();
-	LunMem::getClientInstance()->LocalPlayer->setPlayerGameType(0);
+	if (LunMem::getClientInstance() != NULL) {
+		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
+			LunMem::getClientInstance()->LocalPlayer->setPlayerGameType(0);
+		}
+	}
 }
