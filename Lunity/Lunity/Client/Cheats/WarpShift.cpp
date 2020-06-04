abd90b9c05885371320e78a7807e5ed55a4879e9
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-06-04 13:21:15 +0100

    WarpShift now bypasses the Hive

--- Lunity/Lunity/Client/Cheats/WarpShift.cpp
+++ Lunity/Lunity/Client/Cheats/WarpShift.cpp
@@ -1,10 +1,18 @@
 #include "pch.h"
 #include "WarpShift.h"
 
-Vector3 savedPos;
-
 WarpShift::WarpShift() :Cheat::Cheat("WarpShift", "Movement")
 {
+
+}
+
+void teleportForwards(LocalPlayer* Player) {
+	Vector3 currentPos = *Player->getPos();
+	currentPos.x += cos((Player->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.05f;
+	currentPos.z += sin((Player->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.05f;
+
+	Player->VelocityXYZ.y = (float)0;
+	Player->setPos(&currentPos);
 
 }
 
@@ -14,20 +22,14 @@ void WarpShift::onTick()
 	if (LunMem::getClientInstance() != NULL) {
 		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
 			if (KeyHook::KeyState(0x43)) {
-				Vector3 currentPos = *LunMem::getClientInstance()->LocalPlayer->getPos();
-				currentPos.x += cos((LunMem::getClientInstance()->LocalPlayer->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.05f;
-				currentPos.z += sin((LunMem::getClientInstance()->LocalPlayer->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.05f;
+				LocalPlayer* Player = LunMem::getClientInstance()->LocalPlayer;
 
-				LunMem::getClientInstance()->LocalPlayer->VelocityXYZ.y = (float)0;
-				LunMem::getClientInstance()->LocalPlayer->setPos(&currentPos);
-				MovePlayerPacket* movementPacket = new MovePlayerPacket((Actor*)LunMem::getClientInstance()->LocalPlayer, &savedPos, &LunMem::getClientInstance()->LocalPlayer->LookingVec, 0x1);
+				teleportForwards(Player);
+
+				MovePlayerPacket* movementPacket = new MovePlayerPacket((Actor*)Player, Player->getPos(), &Player->LookingVec, 0x0);
 				LunMem::getClientInstance()->LoopbackPacketSender->sendToServer(movementPacket);
+
 				delete movementPacket;
-			}
-			else {
-				savedPos.x = LunMem::getClientInstance()->LocalPlayer->getPos()->x;
-				savedPos.y = LunMem::getClientInstance()->LocalPlayer->getPos()->y;
-				savedPos.z = LunMem::getClientInstance()->LocalPlayer->getPos()->z;
 			}
 		}
 	}
@@ -36,7 +38,15 @@ void WarpShift::onTick()
 void WarpShift::onPacket(void* Packet, PacketType type, bool* cancel) {
 	if (enabled) {
 		if (KeyHook::KeyState(0x43)) {
-			*cancel = true;
+			if (type == PacketType::Movement) {
+				MovePlayerPacket* currentPacket = (MovePlayerPacket*)Packet;
+				if (currentPacket->onGround == 0x0) {
+					*cancel = false;
+				}
+				else {
+					*cancel = true;
+				}
+			}
 		}
 	}
 }
@@ -44,5 +54,5 @@ void WarpShift::onPacket(void* Packet, PacketType type, bool* cancel) {
 void WarpShift::onEnable()
 {
 	Cheat::onEnable();
-	Logger::log("Hold 'C' to warp!");
+	Logger::log("Hold 'C' to warp");
 }
