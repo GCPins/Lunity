88ff438655ad375346e6d173e8d227fb11d45944
EchoHackCmd <33047114+EchoHackCmd@users.noreply.github.com>
2020-06-05 04:36:34 +0100

    Update HiveGlide.cpp

--- Lunity/Lunity/Client/Cheats/HiveGlide.cpp
+++ Lunity/Lunity/Client/Cheats/HiveGlide.cpp
@@ -21,13 +21,46 @@ void HiveGlide::onTick()
 	if (LunMem::getClientInstance() != NULL) {
 		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
 			LocalPlayer* Player = LunMem::getClientInstance()->LocalPlayer;
-			
-			if (KeyHook::KeyState(0x57)) {
-				Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.3f;
-				Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90.0f) * (3.14159 / 180.0f)) * 0.3f;
+			float calcYaw = Player->LookingVec.y;
+
+			bool pressedWKey = KeyHook::KeyState(0x57);
+			bool pressedSKey = KeyHook::KeyState(0x53);
+			bool pressedAKey = KeyHook::KeyState(0x41);
+			bool pressedDKey = KeyHook::KeyState(0x44);
+
+			if (pressedWKey) {
+				if (!pressedAKey && !pressedDKey) {
+					calcYaw += 90.0f;
+				}
+				else if (pressedAKey) {
+					calcYaw += 45.0f;
+				}
+				else if (pressedDKey) {
+					calcYaw += 135.0f;
+				}
+			}
+			else if (pressedSKey) {
+				if (!pressedAKey && !pressedDKey) {
+					calcYaw -= 90.0f;
+				}
+				else if (pressedAKey) {
+					calcYaw -= 45.0f;
+				}
+				else if (pressedDKey) {
+					calcYaw -= 135.0f;
+				}
+			}
+			else if (!pressedWKey && !pressedSKey) {
+				if (pressedDKey) {
+					calcYaw += 180.0f;
+				}
 			}
 
-			Player->VelocityXYZ.y = (float)0;
+			if (pressedWKey | pressedSKey | pressedAKey | pressedDKey) {
+				Player->VelocityXYZ.x = cos((calcYaw) * (3.14159 / 180.0f)) * 0.3f;
+				Player->VelocityXYZ.y = (float)0;
+				Player->VelocityXYZ.z = sin((calcYaw) * (3.14159 / 180.0f)) * 0.3f;
+			}
 
 			if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - savedTime) >= std::chrono::milliseconds(150)) {
 
