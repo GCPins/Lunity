c83f5e27f12941de774d59d035784e5cbb34d278
Hackerboi69420 <Hackerboi69420@users.noreply.github.com>
2020-06-11 10:57:17 +0200

    ccfly udpoot

--- Lunity/Lunity/Client/Cheats/CCFly.cpp
+++ Lunity/Lunity/Client/Cheats/CCFly.cpp
@@ -9,7 +9,8 @@ CCFly::CCFly() : Cheat::Cheat("CCFly", "Movement")
 
 }
 
-float leCCFlySpeed = .3f;
+float gliderar = 2.0f;
+float leCCFlySpeed = .4f;
 bool ccmoving = false;
 float addBy = 0;
 int ticked = 0;
@@ -49,12 +50,13 @@ void CCFly::onGmTick(GameMode* gm)
 					//Player->animateHurt();
 					Player->swing();
 
-					MovePlayerPacket* pkt = new MovePlayerPacket((Actor*)Player, Player->getPos(), &Player->LookingVec, 1);
-					pkt->Pos.x += cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * 5;
-					pkt->Pos.y += (float)0;
-					pkt->Pos.z += sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * 5;
-					LunMem::getClientInstance()->LoopbackPacketSender->sendToServer(pkt);
-					delete pkt;
+					//MovePlayerPacket* pkt = new MovePlayerPacket((Actor*)Player, Player->getPos(), &Player->LookingVec, 1);
+					//pkt->Pos.x += cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * 5;
+					//pkt->Pos.y += (float)0;
+					//pkt->Pos.z += sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * 5;
+					//LunMem::getClientInstance()->LoopbackPacketSender->sendToServer(pkt);
+					//delete pkt;
+
 
 					Player->swing();
 
@@ -65,12 +67,17 @@ void CCFly::onGmTick(GameMode* gm)
 				if (ccmoving) {
 					//Move the player client
 					Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
-					Player->VelocityXYZ.y = (float)0;
+					Player->VelocityXYZ.y = 0.0f;
 					Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
 
-					if (ticked == 10) {
-						Player->VelocityXYZ.y += .5;
-					}
+						if (ticked == 15) { 
+						ticked = 0;						
+						Vector3 myPosition = *Player->getPos();
+						myPosition.y += 0.5f;
+						Player->setPos(&myPosition);
+						Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * 0.4f;
+						Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * 0.4f;
+						}
 
 					ticked++;
 				}
@@ -83,6 +90,29 @@ void CCFly::onGmTick(GameMode* gm)
 					Player->VelocityXYZ.z = (float)0;
 					//Player->stopSwimming();
 					Logger::log("Stop Moving!");
+				}
+			}
+		}
+	}
+}
+
+void CCFly::onPacket(void* Packet, PacketType type, bool* cancel)
+{
+	if (enabled) {
+		if (ccmoving) {
+			if (type == PacketType::Movement) {
+				if (LunMem::getClientInstance() != NULL) {
+					LocalPlayer* Player = LunMem::getClientInstance()->LocalPlayer;
+					if (Player != NULL) {
+						RakNetInstance* Raknet = LunMem::getClientInstance()->LoopbackPacketSender->NetworkHandler->RakNetInstance;
+						MovePlayerPacket* pkt = (MovePlayerPacket*)Packet;
+						pkt->Pos.y += gliderar;
+						if (ticked == 15)
+						{
+							pkt->Pos.y += 0.5f;
+						}
+						gliderar -= 0.001f;
+					}
 				}
 			}
 		}
