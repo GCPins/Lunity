8cacb25a219ce43d0f2184aba94163a7af700a2e
OvermindMC <Overmind@mail.com>
2020-06-15 11:28:38 +0100

    Customisable Triggerbot Delay

--- Lunity/Lunity/Client/Cheats/Triggerbot.cpp
+++ Lunity/Lunity/Client/Cheats/Triggerbot.cpp
@@ -2,21 +2,29 @@
 #include "Triggerbot.h"
 #include "../Hooks/GamemodeHook.h"
 
+float triggerbotDelayF;
+std::chrono::time_point<std::chrono::steady_clock> oldTime_C = std::chrono::high_resolution_clock::now();
+
 Triggerbot::Triggerbot() :Cheat::Cheat("Triggerbot", "Combat")
 {
-
+	registerSliderSetting("Delay (MS)", &triggerbotDelayF, 0.f, 1000.f);
 }
 
 void Triggerbot::onGmTick(GameMode* gm) {
-	if (LunMem::getClientInstance() != NULL) {
-		if (LunMem::getClientInstance()->LocalPlayer != NULL) {
-			LocalPlayer* player = LunMem::getClientInstance()->LocalPlayer;
-			Actor* lookingAtEnt = LunMem::getClientInstance()->LocalPlayer->MultiPlayerLevel->LookingActor;
-			//logHex("Looking", (ulong)lookingAtEnt);
-			if (lookingAtEnt != NULL) {
-				player->swing();
-				gm->attack(lookingAtEnt);
+	int triggerbotTime = (int)triggerbotDelayF;
+
+	if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - oldTime_C) >= std::chrono::milliseconds{ triggerbotTime }) {
+		if (LunMem::getClientInstance() != NULL) {
+			if (LunMem::getClientInstance()->LocalPlayer != NULL) {
+				LocalPlayer* player = LunMem::getClientInstance()->LocalPlayer;
+				Actor* lookingAtEnt = LunMem::getClientInstance()->LocalPlayer->MultiPlayerLevel->LookingActor;
+				if (lookingAtEnt != NULL) {
+					player->swing();
+					gm->attack(lookingAtEnt);
+				}
 			}
 		}
+
+		oldTime_C = std::chrono::high_resolution_clock::now();
 	}
 }
--- Lunity/Lunity/Client/Cheats/Triggerbot.h
+++ Lunity/Lunity/Client/Cheats/Triggerbot.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../../BigHead.h"
 #include "../Cheat.h"
+#include <chrono>
+
 class Triggerbot : public Cheat
 {
 public:
