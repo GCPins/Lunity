3cadc4dcdf6c71e9ed2cdd1925d31e7f59548032
Hackerboi69420 <Hackerboi69420@users.noreply.github.com>
2020-06-19 14:08:56 +0200

    ...idk lol (ccfly update)

--- Lunity/Lunity/Client/Cheats/CCFly.cpp
+++ Lunity/Lunity/Client/Cheats/CCFly.cpp
@@ -4,20 +4,40 @@
 #include "../../SDK/EntList.h"
 #define PI 3.14159
 
+static std::chrono::time_point<std::chrono::steady_clock> savedTime;
+
 float leCCFlySpeed = 0.25f;
 
 float yee = 1.5f;
 
-CCFly::CCFly():Cheat::Cheat("CCFly", "Movement")
+bool pulse = false;
+
+//float pulseinterval = 0.25;
+
+int Eeeeeee = 1;
+
+float boithesevariablesrbest = 0.001;
+
+bool glide = true;
+
+//bool bump = false;
+
+//float aaaaaaaaaaaaaaaaa = 0.0f;
+
+CCFly::CCFly() :Cheat::Cheat("CCFly", "Movement")
 {
-	registerSliderSetting("Speeeeet", &leCCFlySpeed, 0.0f, 2.0f);
-	registerSliderSetting("yee", &yee, 0.0f, 8.0f);
+	registerSliderSetting("Speed", &leCCFlySpeed, 0.0f, 2.0f);//speed
+	registerSliderSetting("yee", &yee, 0.0f, 8.0f);//how high will it tp u
+	registerToggleSetting("Glide", &glide);//glide
+	registerSliderSetting("Glide", &boithesevariablesrbest, 0.001f, 0.5f);//"glide speed"
+	//registerToggleSetting("Bump", &bump);//more like airjump
+	//registerSliderSetting("Bump", &aaaaaaaaaaaaaaaaa, 0.0f, 2.0f);//bump valocity
+	registerToggleSetting("Pulse", &pulse);//fly with the speed only 500ms else use speed/2
 }
 
 float gliderar;
 bool ccmoving = false;
 float addBy = 0;
-int ticked = 0;
 Vector3 lastPos;
 int posSwitch;
 
@@ -38,6 +58,18 @@ void CCFly::onEnable() {
 	}
 }
 
+void CCFly::onTick() {
+	if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - savedTime) >= std::chrono::milliseconds(50));
+	{
+		Eeeeeee++;
+			if (Eeeeeee > 30) //Eeeeeee > 3
+			{
+				Eeeeeee = 1;
+			}
+		savedTime = std::chrono::high_resolution_clock::now();
+	}
+}
+
 void CCFly::onGmTick(GameMode* gm)
 {
 	if (LunMem::getClientInstance() != NULL) {
@@ -53,31 +85,55 @@ void CCFly::onGmTick(GameMode* gm)
 					Player->swing();
 
 					Logger::log("Moving!");
-					ticked = 0;
 					gliderar = yee;
 				}
 				if (ccmoving) {
 					Player->setSprinting(true);
 					//Move the player client
-					Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
-					Player->VelocityXYZ.y = 0.0f;
-					Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
-						if (ticked == 40) { ticked = 0; }
-					ticked++;
+					if (pulse)
+					{
+						if (Eeeeeee > 20)
+						{
+							Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
+							Player->VelocityXYZ.y = 0.0f;
+							Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
+						}
+						else
+						{
+							Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * (leCCFlySpeed / 2.0f);
+							Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * (leCCFlySpeed / 2.0f);
+						}
+					}
+					else
+					{
+						Player->VelocityXYZ.x = cos((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
+						Player->VelocityXYZ.z = sin((Player->LookingVec.y + 90) * (PI / 180.0f)) * leCCFlySpeed;
+					}
 					if (yee < 0.6f)
 					{
 						yee = 0.6;
 					}
+					if (glide) //& !bump)
+					{
+						Player->VelocityXYZ.y = 0.0f;
+					}
+					/*if (!glide & bump)
+					{
+						if (Eeeeeee == 1)
+						{
+							Player->VelocityXYZ.y = aaaaaaaaaaaaaaaaa;
+						}
+					}*/
 				}
-			}
-			else {
-				if (ccmoving) {
-					ccmoving = false;
-					Player->VelocityXYZ.x = (float)0;
-					Player->VelocityXYZ.y = (float)0;
-					Player->VelocityXYZ.z = (float)0;
-					//Player->stopSwimming();
-					Logger::log("Stop Moving!");
+				else {
+					if (ccmoving) {
+						ccmoving = false;
+						Player->VelocityXYZ.x = (float)0;
+						Player->VelocityXYZ.y = (float)0;
+						Player->VelocityXYZ.z = (float)0;
+						//Player->stopSwimming();
+						Logger::log("Stop Moving!");
+					}
 				}
 			}
 		}
@@ -94,28 +150,27 @@ void CCFly::onPacket(void* Packet, PacketType type, bool* cancel)
 					if (Player != NULL) {
 						RakNetInstance* Raknet = LunMem::getClientInstance()->LoopbackPacketSender->NetworkHandler->RakNetInstance;
 						MovePlayerPacket* pkt = (MovePlayerPacket*)Packet;
-						pkt->Pos.y += gliderar;
-						if (ticked <= 15)
-						{
-							pkt->Pos.y += 0.5f;
-						}
-						else
-						{
-							if (ticked >= 25)
+						if (KeyHook::KeyState(0x46)) {
+							pkt->Pos.y += gliderar;
+							if (Eeeeeee < 10)
+							{
+								pkt->Pos.y += 0.5f;
+							}
+							if (Eeeeeee > 10 & Eeeeeee < 20)
 							{
 								pkt->Pos.y -= 0.5f;
 							}
-							else
+							if (Eeeeeee > 20 & Eeeeeee < 30)
 							{
 								*cancel = true;
 							}
+							gliderar -= boithesevariablesrbest;
+							if (gliderar <= 0.5)
+							{
+								gliderar = (float)yee;
+							}
+							//Logger::log(to_string(gliderar)); debug meh idk
 						}
-						gliderar -= 0.001f;
-						if (gliderar <= 0.5) 
-						{ 
-							gliderar = (float)yee; 
-						}
-						//Logger::log(to_string(gliderar)); debug meh idk
 					}
 				}
 			}
--- Lunity/Lunity/Client/Cheats/CCFly.h
+++ Lunity/Lunity/Client/Cheats/CCFly.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "../Cheat.h"
+#include <chrono>
+
 class CCFly : public Cheat
 {
 public:
 	CCFly();
 	void onGmTick(GameMode* gm);
 	void onEnable();
+	void onTick();
 	void onPacket(void* Packet, PacketType type, bool* cancel);
 };
 
