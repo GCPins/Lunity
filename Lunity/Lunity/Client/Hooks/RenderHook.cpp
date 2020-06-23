645bc38a1c6bc24deda0fa7447ed8cc0b95371b4
DisabledMallis <mallisiscool@gmail.com>
2020-06-23 14:43:45 -0400

    Update RenderHook.cpp

--- Lunity/Lunity/Client/Hooks/RenderHook.cpp
+++ Lunity/Lunity/Client/Hooks/RenderHook.cpp
@@ -8,17 +8,17 @@
 #include <MinHook.h>
 #pragma comment(lib, "libMinHook.lib")
 
-typedef int (WINAPI* RenderText)(__int64 a1, MinecraftUIRenderContext* renderCtx);
+typedef int (__stdcall* RenderText)(__int64 a1, __int64 a2, MinecraftUIRenderContext* renderCtx);
 RenderText original;
 
 float entireW = 0;
-int hookCallback(__int64 a1, MinecraftUIRenderContext* renderCtx) {
+int hookCallback(__int64 a1, __int64 a2, MinecraftUIRenderContext* renderCtx) {
 	DrawUtils::setCtx(renderCtx, LunMem::getClientInstance()->getGuiData());
 
 	CheatManager::onPreRender();
 
 	DrawUtils::flush();
-	int ret = original(a1, renderCtx);
+	int ret = original(a1, a2, renderCtx);
 
 	CheatManager::onPostRender();
 
@@ -28,7 +28,7 @@ int hookCallback(__int64 a1, MinecraftUIRenderContext* renderCtx) {
 
 void RenderHook::installHook() {
 	Logger::log("installing render hook...");
-	void* toHook = (void*)(LunMem::getBaseModule() + 0x7235A0);
+	void* toHook = (void*)(LunMem::getBaseModule() + 0x77F1C0);
 	Logger::logHex("ToHook", (ulong)toHook);
 	bool installSuccess = false;
 	if (MH_CreateHook(toHook, &hookCallback, reinterpret_cast<LPVOID*>(&original)) == MH_OK) {
@@ -45,6 +45,6 @@ void RenderHook::installHook() {
 
 void RenderHook::uninstallHook()
 {
-	void* toHook = (void*)(LunMem::getBaseModule() + 0x7235A0);
+	void* toHook = (void*)(LunMem::getBaseModule() + 0x77F1C0);
 	MH_DisableHook(toHook);
 }
